fccdc452b73b588a423390e2a0cc75e52cea7c6e
liangeek <liangeek@163.com>
2019-03-18 22:09:23 +0800

    invoke

--- read-underscore.js
+++ read-underscore.js
@@ -85,6 +85,29 @@
     return obj != null && hasOwnProperty.call(obj, path);
   }
 
+  var restArguments = function(func, startIndex) {
+    startIndex = startIndex == null ? func.length - 1 : +startIndex;
+    return function() {
+      var length = Math.max(arguments.length - startIndex, 0),
+          rest = Array(length),
+          index = 0;
+      for (; index < length; index++) {
+        rest[index] = arguments[index + startIndex];
+      }
+      switch (startIndex) {
+        case 0: return func.call(this, rest);
+        case 1: return func.call(this, arguments[0], rest);
+        case 2: return func.call(this, arguments[0], arguments[1], rest);
+      }
+      var args = Array(startIndex + 1);
+      for (index = 0; index < startIndex; index++) {
+        args[index] = arguments[index];
+      }
+      args[startIndex] = rest;
+      return func.apply(this, args);
+    };
+  };
+
   // 属性数组，深层查找
   var deepGet = function (obj, path) {
     var length = path.length;
@@ -274,6 +297,27 @@
   _.findWhere = function(obj, attrs) {
     return _.find(obj, _.matcher(attrs));
   };
+
+  _.invoke = restArguments(function(obj, path, args) {
+    var contextPath, func;
+    if (_.isFunction(path)) {
+      func = path;
+    } else if (_.isArray(path)) {
+      contextPath = path.slice(0, -1);
+      path = path[path.length - 1];
+    }
+    return _.map(obj, function(context) {
+      var method = func;
+      if (!method) {
+        if (contextPath && contextPath.length) {
+          context = deepGet(context, contextPath);
+        }
+        if (context == null) return void 0;
+        method = context[path];
+      }
+      return method == null ? method : method.apply(context, args);
+    });
+  });
 
   // object
   _.keys = function (obj) {
