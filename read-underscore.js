c18e35e887c3d2e772c5d3518b4f7f0c3f80c45c
liangeek <liangeek@163.com>
2019-03-29 21:49:20 +0800

    partial

--- read-underscore.js
+++ read-underscore.js
@@ -1000,6 +1000,22 @@
     return self;
   };
 
+  _.partial = restArguments(function(func, boundArgs) {
+    var placeholder = _.partial.placeholder;
+    var bound = function() {
+      var position = 0, length = boundArgs.length;
+      var args = Array(length);
+      for (var i = 0; i < length; i++) {
+        args[i] = boundArgs[i] === placeholder ? arguments[position++] : boundArgs[i];
+      }
+      while (position < arguments.length) args.push(arguments[position++]);
+      return executeBound(func, bound, this, this, args);
+    };
+    return bound;
+  });
+
+  _.partial.placeholder = _;
+
   // todo
   _.bind = restArguments(function (func, context, args) {
     if (!_.isFunction(func)) throw new TypeError('Bind must be called on a function');
